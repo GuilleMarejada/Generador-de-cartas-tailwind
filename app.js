a3c258b148c4e309305b7f5e47d5fe3e4913a610
Guillermo <guillermo.gil.cor@gmail.com>
2024-05-13 17:46:48 +0100

    generador de cartas tailwinds

---
+++ app.js
@@ -0,0 +1,36 @@
+let cardNum = document.querySelector("#number");
+
+const numGenerator = () => {
+  cardNum.innerHTML = Math.floor(Math.random() * 13) + 1;
+  let figures =["As", "J", "Q", "K"];
+  if (cardNum.innerHTML==1){
+    cardNum.innerHTML=figures[0];
+  }else if(cardNum.innerHTML>=11){
+    cardNum.innerHTML=figures[cardNum.innerHTML-10];
+  }
+};
+
+let suits = ["♦", "♥", "♠", "♣"];
+let cardTopSuit = document.querySelector("#top");
+let cardBotSuit = document.querySelector("#bot");
+
+const suitGenerator = () => {
+  cardBotSuit.innerHTML = suits[Math.floor(Math.random() * 4)];
+  cardTopSuit.innerHTML = cardBotSuit.innerHTML;
+};
+
+let aux = 10;
+let contador = document.querySelector("#timer");
+const temporizador = () => {
+  if (aux-- == 1) {
+    init();
+    aux = 10;
+  }
+  contador.innerHTML = aux;
+};
+setInterval(temporizador, 1000);
+
+const init = () => {
+  suitGenerator();
+  numGenerator();
+};
